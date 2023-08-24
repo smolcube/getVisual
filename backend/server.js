7947acf450b772b1e3b986004478a852c456e54d
smolcube <itsrokaia.2016@gmail.com>
2023-08-24 21:55:01 +0200

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv')
 dotenv.config();
+const colors = require("colors")
+const connectDB = require('./config/db');
 const app = express();
 
 const port = process.env.PORT || 5000;
-
+connectDB()
 app.use('/getvisual', require('./routes/mainRoutes'));
 
 app.listen(port, () => {
