41625dac43d40617899c7e20e74df8101c54c8f4
smolcube <itsrokaia.2016@gmail.com>
2023-08-27 04:12:37 +0200

    db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async ()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
+        
+        const dbName = conn.connections[0].name; // Extract the database name
+
+        console.log(`Connected to database: ${dbName}`);
         console.log(`connected successfully : ${conn.connection.host}`.cyan.underline);
 
     } catch (error) {
