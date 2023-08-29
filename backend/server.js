e768df8060aa77a0825cdfe3f1b61291a97336ae
smolcube <itsrokaia.2016@gmail.com>
2023-08-29 05:09:24 +0200

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
 const colors = require("colors")
 const connectDB = require('./config/db');
-const app = express();
+const { errorHandler } = require('./middleware/errorHandler');
 
 const port = process.env.PORT || 5000;
+
 connectDB()
+
+app.use(errorHandler);
+app.use(express.json()); // Use built-in JSON parsing middleware
+app.use(express.urlencoded({ extended: true })); // Use built-in URL-encoded parsing middleware
 app.use('/getvisual', require('./routes/mainRoutes'));
 
 app.listen(port, () => {
