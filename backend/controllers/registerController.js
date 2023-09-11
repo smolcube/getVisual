0cab4f9bd56cec88baf777cd84e7d780ca29cf2c
smolcube <itsrokaia.2016@gmail.com>
2023-09-11 23:15:01 +0200

    controllers

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -21,6 +21,29 @@ const register = asyncHandler(async (req, res) => {
         throw new Error('Please fill all fields');
     }
 
+  // Email validation using a regular expression
+  const emailConditions = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+  if (!emailConditions.test(email)) {
+    res.status(400);
+    throw new Error('Invalid email format');
+  }
+
+   // Password policy regular expression
+   /* At least one uppercase letter
+      At least one lowercase letter
+      At least one digit
+      At least one special character among @$!%*?&
+      Minimum total length of 8 characters */
+  const passwordConditions = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!passwordConditions.test(password)) {
+     res.status(400);
+     throw new Error(
+       'Password must have at least one uppercase letter, one lowercase letter, one digit, one special character among @$!%*?&, and a minimum total length of 8 characters.'
+     );
+   }
+ 
+
     // Check if account exists as a Customer
     const customerExists = await Customer.findOne({ email });
     if (customerExists) {
