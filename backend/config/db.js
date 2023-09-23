b668d7c2f30721d880d378b6a0359434962a4adf
smolcube <itsrokaia.2016@gmail.com>
2023-09-23 22:44:07 +0200

    config

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,9 @@ const connectDB = async ()=>{
         const conn = await mongoose.connect(process.env.MONGO_URI)
         
         const dbName = conn.connections[0].name; // Extract the database name
+        const clusterName = new URL(process.env.MONGO_URI).hostname; // Extract the cluster name
 
+        console.log(`Connected to cluster: ${clusterName}`);
         console.log(`Connected to database: ${dbName}`);
         console.log(`connected successfully : ${conn.connection.host}`.cyan.underline);
 
--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -3,15 +3,10 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config(); // Load environment variables from .env file
 
 // Generate a Toke (JWT)
-function generateJWTToken(id) {
-  const payload = {
-    id: id
-  };
-
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '24hr' // Token expires after 24 hours
-  });
-
+function generateJWTToken(payload, expiresIn) {
+  
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
+  
   return token;
 }
 
