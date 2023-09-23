0f10999e4122b1b11f643fd686c5561cbc64a472
smolcube <itsrokaia.2016@gmail.com>
2023-09-23 22:52:01 +0200

    API

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// import controllers
+//const { main } = require('../controllers/mainController');
+const { dashboard } = require('../controllers/dashboardController');
+const { adminLogin } = require('../controllers/adminController');
+
+const { protect } = require('../middleware/authHandler')
+
+// Main routes
+router.get('/main',protect, dashboard);
+router.post('/login|signin', adminLogin);
+router.post('/logout');
+
+router.get('/:username');
+router.get('/posts');
+router.get('/post/:id');
+router.put('/post/:id');
+router.delete('/post/:id');
+
+module.exports = router;
--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 // import controllers
 const { main} = require('../controllers/mainController');
-const { register } = require('../controllers/registerController');
-const { login } = require('../controllers/loginController')
+const { register, confirmEmail } = require('../controllers/registerController');
+const { login, getProfile } = require('../controllers/loginController')
+const { forgotPass , resetPass} = require('../controllers/passResetingController')
 
 // Main routes
 router.get('/', main);
-router.post('/register|signup',register);
-router.post('/login|signin',login);
+router.post('/register',register);
+router.get('/register/confirm-email/:registerToken',confirmEmail);
+
+router.post('/login',login);
+
+router.post('/login/forgot-password', forgotPass);
+router.post('/login/forgot-password/reset-password/:passwordResetToken', resetPass);
+
+router.get('/:username', getProfile);
+
 
 module.exports = router;
