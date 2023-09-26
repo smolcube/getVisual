89806a5a3ab658e3b73e0c7b1b91bd2cc1ebac0a
smolcube <itsrokaia.2016@gmail.com>
2023-09-26 03:50:31 +0200

    first commit, server setup

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
 const colors = require("colors")
+const cookieParser = require('cookie-parser');
+
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorHandler');
 
 const port = process.env.PORT || 5000;
 
 connectDB()
-
+app.use(cookieParser()); // Use cookie-pasrser to access cookies
 app.use(errorHandler);
 app.use(express.json()); // Use built-in JSON parsing middleware
 app.use(express.urlencoded({ extended: true })); // Use built-in URL-encoded parsing middleware
