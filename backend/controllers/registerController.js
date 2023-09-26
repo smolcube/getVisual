10f4d17c7d929a38d5d05f5c3129c942537de28d
smolcube <itsrokaia.2016@gmail.com>
2023-09-26 05:36:39 +0200

    controllers

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,13 +1,17 @@
 //*** DONE ***//
 const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
+const crypto = require('crypto');
+const cookie = require('cookie');
 
 // Import models
 const Customer = require('../models/customerModel');
 const User = require('../models/userModel');
 
-//JWT module
+// modules
 const generateJWTToken = require('../config/jwt');
+const { isPasswordValid, passwordHashing } = require('../utils/passwordFunc');
+const { sendEmail } = require('../utils/sendEmail');
 
 // @desc   Register new user
 // @route  POST /getvisual/register|signup
@@ -29,20 +33,11 @@ const register = asyncHandler(async (req, res) => {
     throw new Error('Invalid email format');
   }
 
-   // Password policy regular expression
-   /* At least one uppercase letter
-      At least one lowercase letter
-      At least one digit
-      At least one special character among @$!%*?&
-      Minimum total length of 8 characters */
-  const passwordConditions = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  if (!passwordConditions.test(password)) {
-     res.status(400);
-     throw new Error(
-       'Password must have at least one uppercase letter, one lowercase letter, one digit, one special character among @$!%*?&, and a minimum total length of 8 characters.'
-     );
-   }
- 
+  // Check password conditions
+  if (!isPasswordValid(password)) {
+    res.status(400);
+    throw new Error('Password must have at least one uppercase letter, one lowercase letter, one digit, one special character among @$!%*?&, and a minimum total length of 8 characters.');
+  }
 
     // Check if account exists as a Customer
     const customerExists = await Customer.findOne({ email });
@@ -59,8 +54,38 @@ const register = asyncHandler(async (req, res) => {
     }
 
     // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(password, salt);
+    const hashedPass= passwordHashing(password);
+
+
+    // Generate JWT
+    const registerToken = generateJWTToken({ email }, "24hr")
+    const random = crypto.randomBytes(32).toString('hex');
+    
+
+    const registerLink =  `http://localhost:5000/getVisual/register/confirm-email/${random}`
+  
+    // Store the passwordResetToken in a cookie and set its expiration time
+    const tokenCookie = cookie.serialize('registerToken', registerToken, {
+      maxAge: 24 * 60 * 60 * 1000, // 24hr in milliseconds
+      path: '/getVisual/register/confirm-email', // Set the cookie path to match your reset password route
+      httpOnly: true, // Make the cookie accessible only via HTTP
+    });
+
+    // Attach the registerToken cookie to the response
+    res.cookie('registerToken', registerToken);
+
+
+    sendEmail(
+    email,
+    'Email verfication',
+    `Dear ${username},\n\n
+Thank you for registering with getVisual! To complete your registration
+and verify your email address, please click on the link below:
+\n\n${registerLink}\n\nIf you didn't request this registration, 
+please ignore this email.\n\nThank you for choosing getVisual!\n\n
+Sincerely,\nThe getVisual Team`,
+    );
+    res.status(200).json({message:"email sent successfully"});
 
     // Create user OR customer
     let createdAccount;
@@ -81,9 +106,6 @@ const register = asyncHandler(async (req, res) => {
     });
     }
   
-    // Generate JWT
-    const token = generateJWTToken(createdAccount._id);
-
     // check creation
     if (createdAccount) {
     res.status(201).json({
@@ -91,7 +113,7 @@ const register = asyncHandler(async (req, res) => {
       username: createdAccount.username,
       email: createdAccount.email,
       accType: createdAccount.accType,
-      token: token,
+      token: registerToken,
       message: "REGISTERED SUCCESSFULLY" 
     });
     } else {
@@ -100,6 +122,49 @@ const register = asyncHandler(async (req, res) => {
     }
 });
 
+
+// @desc   Confirm email
+// @route  GET /getvisual/register/confirm-email/:registerToken
+// @access Private 
+const confirmEmail = asyncHandler(async (req, res) => {
+  const registerToken = req.cookies.registerToken;
+
+  if (!registerToken) {
+    res.status(400);
+    throw new Error('Invalid token');
+  }
+  
+  try {  
+    // Verify the token and extract the user/customer ID
+    const decodedToken = jwt.verify(registerToken, process.env.JWT_SECRET);
+    const email = decodedToken.email;
+
+    // Update the 'verified' attribute in the User or Customer model
+    const user = await User.findOne({email: email});
+    const customer = await Customer.findOne({email: email});
+
+    if (user) {
+      // Update 'verified' attribute for User
+      user.verified = true;
+      await user.save();
+    } else if (customer) {
+      // Update 'verified' attribute for Customer
+      customer.verified = true;
+      await customer.save();
+    } else {
+      return res.status(404).json({ error: 'User or Customer not found' });
+    }
+
+    res.clearCookie('registerToken');
+
+    return res.status(200).json({ message: 'Email verified successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 module.exports = {
   register,
+  confirmEmail,
 };
