38a944d3c56d76267dc8cd3e0b432eff45d1c990
smolcube <itsrokaia.2016@gmail.com>
2024-02-10 22:00:38 +0200

    server setup

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ const dotenv = require('dotenv')
 dotenv.config();
 const colors = require("colors")
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 const connectDB = require('./config/db');
-const { errorHandler } = require('./middleware/errorHandler');
+const { errorHandler, notFound } = require('./middleware/errorHandler');
 
 const port = process.env.PORT || 5000;
 
 connectDB()
+
+app.use(cors({origin: 'http://localhost:3000', credentials:true}));
 app.use(cookieParser()); // Use cookie-pasrser to access cookies
 app.use(errorHandler);
 app.use(express.json()); // Use built-in JSON parsing middleware
@@ -18,6 +22,10 @@ app.use(express.urlencoded({ extended: true })); // Use built-in URL-encoded par
 
 app.use('/getvisual', require('./routes/mainRoutes'));
 app.use('/getvisual/dashboard', require('./routes/adminRoutes'));
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server runs on port ${port}`);
